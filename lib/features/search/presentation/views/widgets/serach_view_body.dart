cba3266c4b2e00d104520443b15c59294b06c5d2
mohamedelgamalx <muhammadelgamal66@gmail.com>
2025-04-17 18:56:23 +0200

    add search result list view

--- lib/features/search/presentation/views/widgets/serach_view_body.dart
+++ lib/features/search/presentation/views/widgets/serach_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:bookly/features/search/presentation/views/widgets/custom_search_text_field.dart';
+import 'package:bookly/features/search/presentation/views/widgets/search_list_view_result.dart';
 import 'package:flutter/material.dart';
+import '../../../../../core/styles/styles.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
@@ -9,12 +11,15 @@ class SearchViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomSearchTextField(),
+          const SizedBox(height: 16),
+          Text('Search Results', style: Styles.titleMedium18),
+          const SizedBox(height: 16),
+          Expanded(child: SearchResultListView()),
         ],
       ),
     );
   }
 }
-
-
